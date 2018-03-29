e610c0748cbf429526661c2c6adc9190550da085
ShellyShellyShelly <1543829197@qq.com>
2018-03-29 13:44:13 +0800

    2018-03-29

---
+++ ResumeManagementSystem/Scripts/Resume/Index.js
@@ -0,0 +1,45 @@
+﻿var dataList;
+$(function () {
+
+    InitDataList();
+    $(".showList").click(function () {
+        $(".cardStyle").css("display", "none");
+        $(".listStyle").css("display", "block");
+    });
+
+    $(".showCard").click(function () {
+        $(".listStyle").css("display", "none");
+        $(".cardStyle").css("display", "block");
+    });
+
+    $(".showList").click(function () {
+        $("#tblContent2").html($("#contentTemplate2").render(dataList));
+    });
+});
+
+function InitDataList(page) {
+
+    var expectPosition = $(".expectPosition").val() == null ? null : $(".expectPosition").val().trim();//期望职位值
+    var degree = $(".degree").val() == null ? null : $(".degree").val().trim();//学历值
+    var gender = $(".gender").val() == null ? null : $(".gender").val().trim();//性别值
+    var name = $(".name").val() == null ? null : $(".name").val().trim();//姓名值
+    var address = $(".address").val() == null ? null : $(".address").val().trim();//地址值
+    var age = $(".age").val() == null ? null : $(".age").val().trim();//年龄值
+    var workExperience = $(".workExperience").val() == null ? null : $(".workExperience").val().trim();//工作经验值
+    var company = $(".company").val() == null ? null : $(".company").val().trim();//公司值
+    var position = $(".position").val() == null ? null : $(".position").val().trim();//目前职位值
+
+    $.post(
+         "/Resume/GetResumeList",
+        { expectPosition: expectPosition, degree: degree, gender: gender, name: name, address: address, age: age, workExperience: workExperience, company: company, position: position, pageNo: 1, size: 20 },
+        function (data, status) {
+            //数据写到前台
+            dataList = data.dataList;
+            $("#tblContent1").html($("#contentTemplate1").render(dataList));
+            //$("#tblContent2").html($("#contentTemplate2").render(data.dataList));
+            //$("#divPage").html(data["pageList"]);
+        },
+    "json"
+    );
+}
+
---
+++ ResumeManagementSystem/Scripts/Resume/ResumeDetail.js
@@ -0,0 +1,65 @@
+﻿$(function () {
+    credit();
+    $(".showList").click(function () {
+        $(".cardStyle").css("display", "none");
+        $(".listStyle").css("display", "block");
+    });
+
+    $(".showCard").click(function () {
+        $(".listStyle").css("display", "none");
+        $(".cardStyle").css("display", "block");
+    });
+
+    $(".containerimg").mousemove(function () {
+        $(".containerimg .resume-tips ul").css("transform", "rotateX(1.16deg) rotateY(6.2deg)");
+    });
+    $(".containerimg").mouseout(function () {
+        $(".containerimg .resume-tips ul").css("transform", "rotateX(-4.24deg) rotateY(-4.88deg)");
+    });
+});
+
+function credit() {
+    var chart = AmCharts.makeChart("credit", {
+        type: "radar",
+        theme: "light",
+        fontFamily: "Open Sans",
+        color: "#888",
+        dataProvider: [{
+            country: "身份认证",
+            litres: 156.9
+        }, {
+            country: "简历质量",
+            litres: 131.1
+        }, {
+            country: "行为偏好",
+            litres: 115.8
+        }, {
+            country: "信用历史",
+            litres: 109.9
+        }, {
+            country: "社交人脉",
+            litres: 108.3
+        }],
+        valueAxes: [{
+            axisTitleOffset: 20,
+            minimum: 0,
+            axisAlpha: .15
+        }],
+        startDuration: 2,
+        graphs: [{
+            balloonText: "[[value]] litres of beer per year",
+            bullet: "round",
+            valueField: "litres"
+        }],
+        categoryField: "country",
+        exportConfig: {
+            menuTop: "10px",
+            menuRight: "10px",
+            menuItems: [{
+                icon: "/lib/3/images/export.png",
+                format: "png"
+            }]
+        }
+    });
+    chart.write('credit');
+}
